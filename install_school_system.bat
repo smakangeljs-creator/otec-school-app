91765f2372e77948b8cb51bc69d4f1dc164d8d42
smakangeljs-creator <smak.angel.js@gmail.com>
2026-07-29 16:57:29 +0300

    Fix install_school_system.bat: improve git clone and auto-launch application

--- install_school_system.bat
+++ install_school_system.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 echo ========================================================
 echo       OTEC School Management System - Auto Installer      
 echo ========================================================
@@ -7,32 +8,31 @@ echo.
 set TARGET_DIR=School Management System
 
 if not exist "%TARGET_DIR%" (
-  echo [1/4] Creating folder "%TARGET_DIR%"...
-  mkdir "%TARGET_DIR%"
+  echo [1/4] Downloading system files from GitHub...
+  git clone https://github.com/smakangeljs-creator/otec-school-app.git "%TARGET_DIR%"
 ) else (
   echo [1/4] Folder "%TARGET_DIR%" already exists.
 )
 
 cd "%TARGET_DIR%"
 
-if not exist ".git" (
-  echo [2/4] Downloading latest system files from GitHub...
-  git clone https://github.com/smakangeljs-creator/otec-school-app.git .
+if exist ".git" (
+  echo [2/4] Syncing latest updates...
+  git fetch origin
+  git reset --hard origin/main
 ) else (
-  echo [2/4] System files already downloaded. Pulling latest updates...
-  git pull origin main
+  echo [2/4] Directory is not a git repository. Skipping sync.
 )
 
 echo.
 echo [3/4] Installing necessary dependencies (this may take a few minutes)...
-call npm install
+call npm install --force
 
 echo.
-echo [4/4] Everything is ready! Starting the server...
+echo [4/4] Everything is ready! Starting the server and opening the application...
 echo.
-echo IMPORTANT: Once the server starts, you can access it on this machine at:
-echo http://localhost:5000
-echo Or on your phone/other computers by checking the "Network" IP shown below.
+echo The application should automatically launch in a few seconds...
 echo.
 
-call npm run dev -- --host
+:: If you want the web browser version, you can change this to: call npm run dev
+call npm run desktop:dev
